a66cc5c49f44970a525e675290fff41b97020719
anhkiet190999 <anhkiet190999@gmail.com>
2022-09-08 21:31:57 -0500

    part b partial

--- fall_2022/computer netwrok/multi-thread web server/src/HttpRequest.java
+++ fall_2022/computer netwrok/multi-thread web server/src/HttpRequest.java
@@ -1,9 +1,7 @@
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.Socket;
 import java.security.cert.CRL;
+import java.util.StringTokenizer;
 
 final class HttpRequest implements Runnable{
 
@@ -45,12 +43,84 @@ final class HttpRequest implements Runnable{
             System.out.println(headerLine);
         }
 
+        //extract the file name from the request line
+        StringTokenizer tokens = new StringTokenizer(requestLine);
+        tokens.nextToken(); //skip over the method, which should be "GET"
+        String fileName = tokens.nextToken();
+        //Because the browser precedes the filename with a slash, we prefix a dot so that the resulting pathname starts within the current directory.
+
+        //open the requested file
+        FileInputStream fileInputStream = null;
+        boolean fileExists = true;
+        try {
+            fileInputStream = new FileInputStream(fileName);
+        }catch (FileNotFoundException e){
+            fileExists = false;
+        }
+
+        // Construct the response message.
+        String statusLine = null;
+        String contentTypeLine = null;
+        String entityBody = null;
+        if (fileExists) {
+            statusLine = tokens.nextToken(); //????
+            contentTypeLine = "Content-type: " +
+                    contentType( fileName ) + CRLF;
+        } else {
+            statusLine = CRLF; //?????
+            contentTypeLine = CRLF; //????
+            entityBody = "<HTML>" +
+                    "<HEAD><TITLE>Not Found</TITLE></HEAD>" +
+                    "<BODY>Not Found</BODY></HTML>";
+        }
+        // Send the status line.
+        dataOutputStream.writeBytes(statusLine);
+
+        // Send the content type line.
+        dataOutputStream.writeBytes(contentTypeLine);
+
+        // Send a blank line to indicate the end of the header lines.
+        dataOutputStream.writeBytes(CRLF);
+
+        // Send the entity body.
+        if (fileExists)	{
+            sendBytes(fileInputStream, dataOutputStream);
+            fileInputStream.close();
+        } else {
+            dataOutputStream.writeBytes(entityBody);
+        }
+
         //close the streams and socket
         dataOutputStream.close();
         bufferedReader.close();
         socket.close();
+    }
+
+    private static String contentType(String fileName)
+    {
+        if(fileName.endsWith(".htm") || fileName.endsWith(".html")) {
+            return "text/html";
+        }
+//        if() {
+//		?;
+//    }
+//        if(?) {
+//		?;
+//    }
+        return "application/octet-stream";
+    }
 
 
+    private static void sendBytes(FileInputStream fis, OutputStream os)
+            throws Exception
+    {
+        // Construct a 1K buffer to hold bytes on their way to the socket.
+        byte[] buffer = new byte[1024];
+        int bytes = 0;
 
+        // Copy requested file into the socket's output stream.
+        while((bytes = fis.read(buffer)) != -1 ) {
+            os.write(buffer, 0, bytes);
+        }
     }
 }
