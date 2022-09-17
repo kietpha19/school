8d709fd1dcef44f80caaa8b7711c1e44c9e43b20
kiet pha <85252510+anhkiet190999@users.noreply.github.com>
2022-09-17 00:29:13 -0500

    http project: break down to smaller functions

--- fall_2022/computer netwrok/multi-thread web server/src/HttpRequest.java
+++ fall_2022/computer netwrok/multi-thread web server/src/HttpRequest.java
@@ -1,12 +1,13 @@
 import java.io.*;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
-import java.security.cert.CRL;
+import java.util.HashSet;
 import java.util.StringTokenizer;
 
 final class HttpRequest implements Runnable{
 
     final static String CRLF = "\r\n";
+    final static String http_version = "HTTP/1.0 ";
     Socket socket;
 
     //constructor
@@ -16,7 +17,7 @@ final class HttpRequest implements Runnable{
     @Override
     public void run() {
         try{
-            //System.out.println("process request");
+            //System.out.println("process request"); // for debugging
             processRequest();
         }catch (Exception e){
             System.out.println(e);
@@ -24,16 +25,18 @@ final class HttpRequest implements Runnable{
     }
 
     private void processRequest() throws Exception{
-        //get a reference to the socket's input and output streams
-        InputStream inputStream =  socket.getInputStream();
+        //create an outputSTream from the socket
         DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
 
+        //get a reference to the socket's input and output streams
+        InputStream inputStream =  socket.getInputStream();
         //set up input stream filters
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-
         //get the request line of the HTTP request message
         String requestLine = bufferedReader.readLine();
 
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        //these lines of code for display to the console at server side for debugging purpose
         //display the request line
         System.out.println();
         System.out.println(requestLine);
@@ -44,56 +47,25 @@ final class HttpRequest implements Runnable{
         while((headerLine = bufferedReader.readLine()).length() != 0){
             System.out.println(headerLine);
         }
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 
         //extract the file name from the request line
         StringTokenizer tokens = new StringTokenizer(requestLine);
         tokens.nextToken(); //skip over the method, which should be "GET"
         String fileName = tokens.nextToken();
-        //Because the browser precedes the filename with a slash, we prefix a dot so that the resulting pathname starts within the current directory.
-        fileName = "./src" + fileName;
 
-        //open the requested file
-        FileInputStream fileInputStream = null;
-        boolean fileExists = true;
-        try {
-            fileInputStream = new FileInputStream(fileName);
-        }catch (FileNotFoundException e){
-            fileExists = false;
+
+        HashSet<String> pages = getAvailablePages();
+        String statusLine = getStatusLine(pages, fileName);
+        if(statusLine == "404 Not Found"){
+            fetchNotFound(dataOutputStream);
+        }
+        else{
+            fetch_page(statusLine, fileName, dataOutputStream);
         }
 
-        // Construct the response message.
-        String statusLine = null;
-        String contentTypeLine = null;
-        String entityBody = null;
-        if (fileExists) {
-            statusLine = "HTTP/1.0 200 OK" + CRLF; //????
-            contentTypeLine = "Content-Type: " +
-                    contentType( fileName ) + CRLF;
-            //content-type: text/html; charset=utf-8
-        } else {
-            statusLine = "404 Not Found" + CRLF; //?????
-            contentTypeLine = "content-type: text/html" + CRLF; //????
 
-            entityBody = "<HTML>" +
-                    "<HEAD><TITLE>Not Found</TITLE></HEAD>" +
-                    "<BODY>Not Found</BODY></HTML>";
-        }
-        // Send the status line.
-        dataOutputStream.write(statusLine.getBytes());
-
-        // Send the content type line.
-        dataOutputStream.write(contentTypeLine.getBytes());
-
-        // Send a blank line to indicate the end of the header lines.
-        dataOutputStream.writeBytes(CRLF);
-
-        // Send the entity body.
-        if (fileExists)	{
-            sendBytes(fileInputStream, dataOutputStream);
-            fileInputStream.close();
-        } else {
-            dataOutputStream.writeBytes(entityBody);
-        }
         dataOutputStream.flush();
         //close the streams and socket
         dataOutputStream.close();
@@ -101,21 +73,87 @@ final class HttpRequest implements Runnable{
         socket.close();
     }
 
+    private static void fetch_page(String statusLine, String fileName, DataOutputStream dataOutputStream) throws IOException {
+        FileInputStream fileInputStream = null;
+        if(statusLine == "200 OK"){
+            //Because the browser precedes the filename with a slash, we prefix a dot so that the resulting pathname starts within the src directory.
+            fileName = "./src/pages" + fileName;
+        }
+        else if(statusLine == "301 Moved Permanently"){
+            fileName = "./src/pages/home.html";
+        }
+        //open the requested file
+        try {
+            fileInputStream = new FileInputStream(fileName);
+        }catch (FileNotFoundException e){
+            fetchNotFound(dataOutputStream);
+        }
+        statusLine = http_version + statusLine + CRLF;
+        String contentTypeLine = "Content-Type: " + contentType( fileName ) + CRLF;
+        try {
+            dataOutputStream.writeBytes(statusLine);
+            dataOutputStream.writeBytes(contentTypeLine);
+            // Send a blank line to indicate the end of the header lines.
+            dataOutputStream.writeBytes(CRLF);
+            sendBytes(fileInputStream,dataOutputStream);
+        }
+        catch (Exception e){
+            fetchNotFound(dataOutputStream);
+        }
+        fileInputStream.close();
+    }
+
+    private static void fetchNotFound(DataOutputStream dataOutputStream) {
+        String statusLine = http_version + "404 Not Found" + CRLF;
+        String contentTypeLine = "Content-Type: text/html" + CRLF;
+
+        String entityBody = "<HTML>" +
+                    "<HEAD><TITLE>Not Found</TITLE></HEAD>" +
+                    "<BODY>Not Found</BODY></HTML>";
+        try {
+            dataOutputStream.writeBytes(statusLine);
+            dataOutputStream.writeBytes(contentTypeLine);
+            // Send a blank line to indicate the end of the header lines.
+            dataOutputStream.writeBytes(CRLF);
+            dataOutputStream.writeBytes(entityBody);
+        }catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    private static String getStatusLine(HashSet<String> pages, String fileName){
+        if(fileName.equals("/index.html")){
+            return "301 Moved Permanently";
+        }
+        else if(pages.contains(fileName)){
+            return "200 OK";
+        }
+        return "404 Not Found";
+    }
+
+    //this function return a hashset that contain all available file name that can be fetched from GET funtion in http
+    private static HashSet<String> getAvailablePages(){
+        HashSet<String> pages = new HashSet<>();
+        String path_name = System.getProperty("user.dir") + "/src/pages";
+        File dir_pages = new File(path_name);
+        File[] files = dir_pages.listFiles();
+        for(File file : files){
+            pages.add("/" + file.getName());
+        }
+        return pages;
+    }
+
+    //this function for now only handle html file,
+    //if need to handle more different file type, can add more if condition
     private static String contentType(String fileName)
     {
         if(fileName.endsWith(".htm") || fileName.endsWith(".html")) {
-            return "text/html;";
+            return "text/html";
         }
-//        if() {
-//		?;
-//    }
-//        if(?) {
-//		?;
-//    }
         return "application/octet-stream";
     }
 
-
+    //this function take all content from file and write it into a DataOutputStream
     private static void sendBytes(FileInputStream fis, DataOutputStream os)
             throws Exception
     {
--- fall_2022/computer netwrok/multi-thread web server/src/WebServer.java
+++ fall_2022/computer netwrok/multi-thread web server/src/WebServer.java
@@ -5,11 +5,16 @@ import java.util.*;
 public class WebServer {
     private static int port = 8080;
     public static void main(String args[]) throws Exception{
+        //create a server socket that listening to request from clients
         ServerSocket serverSocket = new ServerSocket(port);
 
         while(true) {
+            //accept the request and create new sockets for this particular client
             Socket connectionSocket = serverSocket.accept();
+            //create a new http request object to handle the request from client
             HttpRequest request = new HttpRequest(connectionSocket);
+
+            //create and start a new separated thread for this request
             Thread thread = new Thread(request);
             thread.start();
         }
