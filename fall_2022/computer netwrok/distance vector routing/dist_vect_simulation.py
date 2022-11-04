da12c0943a7a9b7fc27b73673873a416cc67aeb6
anhkiet190999 <anhkiet190999@gmail.com>
2022-11-03 23:01:02 -0500

    Create dist_vect_simulation.py

---
+++ fall_2022/computer netwrok/distance vector routing/dist_vect_simulation.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+#create tkinter window
+win = Tk()
+win.geometry("500x500")
+win.title('Distance Vector Routing Simulation')
+root = Frame(win)
+root.pack(side="top", expand=True, fill="both")
+
+win.mainloop()
